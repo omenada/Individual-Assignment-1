2591d238b19c3205a6cdf82dc249be018f838a8f
Daniela Omena <rdaniglad@hotmail.com>
2019-05-24 18:21:49 -0400

    commit 2

--- Game/Game/Form2.cs
+++ Game/Game/Form2.cs
@@ -10,23 +10,31 @@ using System.Windows.Forms;
 
 namespace Game
 {
-    public partial class Form2 : Form
+    public partial class Form2 : Form 
     {
         public Form2()
         {
             InitializeComponent();
+           
         }
 
         private void BtnStartGame_Click(object sender, EventArgs e)
         {
-            Form1 mainGame = new Form1();// intanciate the form 1 to find the label  
-            if (txtplayer1.Text == "")
+            if (txtplayer1.Text == "") // validation to not accept empty value
+            {
                 MessageBox.Show("Enter your name.");
+                
+            }
             else
+            {
+                Form1 mainGame = new Form1();// intanciate the form 1 to find the label  
+                mainGame.lblPlayer1.Text = txtplayer1.Text;
+                mainGame.Show();//call to show the other form
 
-            mainGame.lblPlayer1.Text = txtplayer1.Text;
-            mainGame.Show();//call to show the other form
-        }
+            }
+           
+           
+         }
 
         
     }
